6f777484ed4c4478ed522ae078013560d840b18d
Ethan Bork <ebork11@gmail.com>
2026-06-13 20:45:46 -0700

    progress on implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::time::SystemTime;
+
+use chrono::{DateTime, Local};
+use clap::{Parser, Subcommand};
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    if let Some(command) = cli.command {
+        match command {
+            Commands::Prompt { right, dir } => {
+                if right {
+                    prompt_right();
+                } else {
+                    prompt(dir.unwrap_or_default());
+                }
+            },
+        }
+    }
+}
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Prompt {
+        #[arg(long, default_value_t = false)]
+        right: bool,
+
+        #[arg(long)]
+        dir: Option<String>,        
+    },
+}
+
+fn prompt(dir: String) {
+    println!("{}> ", dir); 
+}
+
+fn prompt_right() {
+    let system_time = SystemTime::now();
+    let datetime: DateTime<Local> = system_time.into();
+
+    println!("{}", datetime.format("%T"));
 }
