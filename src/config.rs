e09a0281f056c04760dbc8c8664bb082539f84c3
Ethan Bork <ebork11@gmail.com>
2026-06-20 12:46:24 -0700

    include default terminal format

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,7 @@ use serde::Deserialize;
 
 use crate::modules;
 
-#[derive(Deserialize, Default)]
+#[derive(Deserialize)]
 pub(crate) struct Config {
     pub(crate) format: String,
     pub(crate) dir: Option<modules::dir::DirModuleConfig>,
@@ -20,4 +20,15 @@ pub(crate) fn parse_config(home_dir: &str) -> Config {
     let config_file = std::fs::read_to_string(config_path).unwrap_or_default();
     let config: Config = toml::from_str(&config_file).unwrap_or_default();
     config
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self { 
+            format: "[dir][fill][time]\n[indicator] ".to_string(),
+            dir: Default::default(), 
+            time: Default::default(), 
+            fill: Default::default() 
+        }
+    }
 }
